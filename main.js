ad44f58ba91466012f9b1d7b0112c05e6806c046
raypark0 <raymwpark@gmail.com>
2021-01-24 21:39:07 +0900

    2

--- main.js
+++
@@ -1,7 +0,0 @@
-const toggleBtn = document.querySelector('.navbar_toggleBtn');
-const menu=document.querySelector('.navbar_menu')
-const icons=document.querySelector('.navbar_icons')
-
-toggleBtn.addEventListener('click',()=>{
-  menu.classList.toggle('active');
-})
